7463c29cb4f71630e5c8352d3af61a5a607ee780
shankhadeepde <shankhadeep.de@in.pwc.com>
2016-09-23 12:02:39 +0530

    SQL dependency changed to mongoDB

--- Router/authRoute.js
+++ Router/authRoute.js
@@ -3,38 +3,38 @@ var router = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var passport = require('passport');
 
-var authRouter = function(){
+var authRouter = function () {
       router.route('/signup')
-            .post(function(req,res){
-               console.log(req.body);
-               var url = 'mongodb://localhost:27017/libraryApp';
-               mongodb.connect(url,function(err,db){
-                   var collection = db.collection('users');
-                   var user = {
-                       username : req.body.userName,
-                       password : req.body.password
-                     };
-                   collection.insert(user,function(err, results){
-                     req.login(results.ops[0],function(){
-                     res.redirect('/auth/Profile');
-                     });
-                   });
-               });               
+            .post(function (req, res) {
+                  console.log(req.body);
+                  var url = 'mongodb://localhost:27017/libraryApp';
+                  mongodb.connect(url, function (err, db) {
+                        var collection = db.collection('users');
+                        var user = {
+                              username: req.body.userName,
+                              password: req.body.password
+                        };
+                        collection.insert(user, function (err, results) {
+                              req.login(results.ops[0], function () {
+                                    res.redirect('/auth/Profile');
+                              });
+                        });
+                  });
             });
 
-      router.route('/signin')      
-            .post(passport.authenticate('local',{
-                  failureRedirect:'/'
-            }),function(req,res){
-                  res.redirect('/auth/Profile');                  
+      router.route('/signin')
+            .post(passport.authenticate('local', {
+                  failureRedirect: '/'
+            }), function (req, res) {
+                  res.redirect('/auth/Profile');
             });
 
       router.route('/Profile')
-            .get(function(req,res){
-               res.json(req.user);
+            .get(function (req, res) {
+                  res.json(req.user);
             });
 
-      return router;      
+      return router;
 }
 
 module.exports = authRouter;
--- Router/bookRoute.js
+++ Router/bookRoute.js
@@ -2,38 +2,40 @@ var express = require('express');
 
 var router = express.Router();
 
-var sql = require('mssql');
+var mongodb = require('mongodb').MongoClient;
 
 
-var bookRouter = function(data){
-          router.use(function(req,res,next){
-                    if(!req.user){
-                        console.log('only members can view the book list');
-                        res.redirect('/Home');
-                      }
-                   else{ next()};
-          });
-          router.route('/')
-                .get(function(req,res){
-                   // var request = new sql.Request();
-                   // request.query('select * from WorkItemInformation',function(err,dataset){
-                   //      res.send(dataset);
-                   //  });
-                      res.send(data);
-                   });
+var bookRouter = function (data) {
+    router.use(function (req, res, next) {
+        if (!req.user) {
+            console.log('only members can view the book list');
+            res.redirect('/Home');
+        }
+        else { next(); }
+    });
+    router.route('/')
+        .get(function (req, res) {
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.find({}).toArray(function (err, results) {
+                    res.send(results);
+                });
+            });
+        });
 
-          router.route('/:id')
-                .get(function(req,res){
-                    var request = new sql.Request();
-                    var ps = new sql.PreparedStatement();
-                    ps.input('id',sql.Int);
-                    ps.prepare('select * from WorkItemInformation where id = @id',function(err){
-                        ps.execute({id:req.params.id}, function(err,workItem){
-                            res.send(workItem);
-                        })
-                    });
-                   });       
+    router.route('/:id')
+        .get(function (req, res) {
+            var request = new sql.Request();
+            var ps = new sql.PreparedStatement();
+            ps.input('id', sql.Int);
+            ps.prepare('select * from WorkItemInformation where id = @id', function (err) {
+                ps.execute({ id: req.params.id }, function (err, workItem) {
+                    res.send(workItem);
+                });
+            });
+        });
 
-        return router;
+    return router;
 }
 module.exports = bookRouter;
